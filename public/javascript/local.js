d2fdf50b86f1eca87f41396f9efc4ac63b078e9e
talipapa <54449218+RetroGhosty@users.noreply.github.com>
2023-04-07 23:22:37 +0800

    added hero

---
+++ public/javascript/local.js
@@ -0,0 +1,14 @@
+const burgerBtn = document.querySelector('.burger')
+const mobileNav = document.querySelector('.mobile-nav')
+
+burgerBtn.addEventListener('click', () => {
+    burgerBtn.classList.toggle('d-lg-none')
+    mobileNav.classList.toggle('visible')
+})
+
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header')
+    header.classList.toggle('sticky', window.scrollY > 460);
+    mobileNav.classList.toggle('sticky', window.scrollY > 460);
+
+})
--- public/javascript/navbarAdjust.js
+++
@@ -1,7 +0,0 @@
-const burgerBtn = document.querySelector('.burger')
-const mobileNav = document.querySelector('.mobile-nav')
-
-burgerBtn.addEventListener('click', () => {
-    burgerBtn.classList.toggle('d-md-none')
-    mobileNav.classList.toggle('visible')
-})
